f56eb65dd630babffa7c1da37ad21ee6c9f76468
Charles Wiredu <afrowebs@gmail.com>
2017-07-02 19:31:13 +0100

    tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ActiveRecord::Base
-  has_many :tags, through: :taggings
-  has_many :taggings, dependent: :destroy
+  #has_many :tags, through: :taggings
+  #has_many :taggings, dependent: :destroy
 end
